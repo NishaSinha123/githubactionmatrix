1ffa5c8ed5fdfe32796848556175b4fd4bac4551
Nisha Sinha <sinhagautam488@gmail.com>
2026-05-31 00:39:17 +0530

    third time

--- index.test.js
+++ index.test.js
@@ -1,17 +1,10 @@
+
 const { add, multiply } = require('./index');
 
-test('adds 2 + 3 to equal 5', () => {
+test('2 + 3 should be 5', () => {
     expect(add(2, 3)).toBe(5);
 });
 
-test('multiplies 4 × 5 to equal 20', () => {
+test('4 × 5 should be 20', () => {
     expect(multiply(4, 5)).toBe(20);
-});
-
-test('adds negative numbers', () => {
-    expect(add(-2, -3)).toBe(-5);
-});
-
-test('multiplies by zero', () => {
-    expect(multiply(5, 0)).toBe(0);
 });
